e0efbd2956494aa508fca1ab23256fb986e4af12
cartooncartoon <46901964+cartooncartoon@users.noreply.github.com>
2020-10-28 09:22:58 -0400

    main

--- server/index.js
+++ server/index.js
@@ -14,27 +14,15 @@ const io = socketio(server);
 app.use(cors());
 app.use(router);
 
+
+
 io.on('connect', (socket) => {
-  socket.on('join', ({ name, room }, callback) => {
-    const { error, user } = addUser({ id: socket.id, name, room });
-
-    if(error) return callback(error);
-
-    socket.join(user.room);
-
-    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
-    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
-
-    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
-
-    callback();
-  });
-
+ 
   socket.on('sendMessage', (message, callback) => {
     const user = getUser(socket.id);
     const timestamp = new Date().getTime();
 
-    io.to(user.room).emit('message', { user: user.name, text: message, timestamp });
+    socket.broadcast.emit('message', { user: user, text: message, timestamp });
 
     callback();
   });
@@ -43,8 +31,7 @@ io.on('connect', (socket) => {
     const user = removeUser(socket.id);
 
     if(user) {
-      io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
-      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+      socket.emit('message', { user: 'Admin', text: `${user} has left.` });
     }
   })
 });
